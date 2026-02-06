851d36183a4d2fb5e8c4917201889b723f27e108
mannazentrum01 <mannazentrum01@gmail.com>
2026-02-06 10:07:32 +0000

    build: suppress chunk size warning

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,9 @@ export default defineConfig({
     port: 3000,
     host: '0.0.0.0',
   },
+  build: {
+    chunkSizeWarningLimit: 1000,
+  },
   plugins: [react(), visualizer({ open: true })],
   resolve: {
     alias: {
